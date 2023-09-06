9909d89dc42c8922b9b27e78d4101e28f16e1759
jvlover <122508461+jvlover@users.noreply.github.com>
2023-09-06 19:54:24 +0900

    Update 경비원.java

--- 백준/Silver/2564. 경비원/경비원.java
+++ 백준/Silver/2564. 경비원/경비원.java
@@ -1,43 +1,53 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static class Store {
+        int r, c;
+        Store(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken())+1;
-        int M = Integer.parseInt(st.nextToken())+1;
-        int shop = Integer.parseInt(br.readLine());
-        int[][] shops = new int[shop+1][2];
-        for (int i = 0; i < shop+1; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int temp = Integer.parseInt(st.nextToken());
-        	if (temp == 1) {
-        		shops[i][0] = 0;
-        		shops[i][1] = Integer.parseInt(st.nextToken());
-        	} else if (temp == 2) {
-        		shops[i][0] = M-1;
-        		shops[i][1] = Integer.parseInt(st.nextToken());
-        	} else if (temp == 3) {
-        		shops[i][0] = Integer.parseInt(st.nextToken());
-        		shops[i][1] = 0;
-        	} else if (temp == 4) {
-        		shops[i][0] = Integer.parseInt(st.nextToken());
-        		shops[i][1] = N-1;
-        	}
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int store = Integer.parseInt(br.readLine());
+        Store[] stores = new Store[store+1];
+        for (int i = 0; i <= store; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int dir = Integer.parseInt(st.nextToken());
+            int point = Integer.parseInt(st.nextToken());
+            switch (dir) {
+                case 1:
+                    stores[i] = new Store(0, point);
+                    break;
+                case 2:
+                    stores[i] = new Store(M, point);
+                    break;
+                case 3:
+                    stores[i] = new Store(point, 0);
+                    break;
+                case 4:
+                    stores[i] = new Store(point, N);
+            }
         }
         int sum = 0;
-        for (int i = 0; i < shop; i++) {
-        	if (Math.abs(shops[i][0] - shops[shop][0]) == M-1)
-        		sum += M-1 + Math.min(shops[i][1]+shops[shop][1], 2*(N-1)-shops[i][1]-shops[shop][1]);
-        	else if (Math.abs(shops[i][1] - shops[shop][1]) == N-1)
-        		sum += N-1 + Math.min(shops[i][0]+shops[shop][0], 2*(M-1)-shops[i][0]-shops[shop][0]);
-        	else if (shops[i][0] == shops[shop][0]) 
-        		sum += Math.abs(shops[i][1] - shops[shop][1]);
-        	else if (shops[i][1] == shops[shop][1]) 
-        		sum += Math.abs(shops[i][0] - shops[shop][0]);
-        	else
-        		sum += Math.abs(shops[i][0]-shops[shop][0]) + Math.abs(shops[i][1]-shops[shop][1]);
+        Store person = stores[store];
+        for (int i = 0; i < store; ++i) {
+            Store shop = stores[i];
+            if (Math.abs(shop.r - person.r) == M)
+                sum += M + Math.min(shop.c + person.c, 2*N - shop.c - person.c);
+            else if (Math.abs(shop.c -person.c) == N)
+                sum += N + Math.min(shop.r + person.r, 2*M - shop.r - person.r);
+            else if (shop.r == person.r)
+                sum += Math.abs(shop.c - person.c);
+            else if (shop.c == person.c)
+                sum += Math.abs(shop.r - person.r);
+            else
+                sum += Math.abs(shop.c - person.c) + Math.abs(shop.r - person.r);
         }
-        System.out.println(sum);
+        System.out.print(sum);
     }
 }
