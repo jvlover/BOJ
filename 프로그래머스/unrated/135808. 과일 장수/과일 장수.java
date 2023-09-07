84e8c13ea5f65500e33de398c3b94ba57bc3e5c7
jvlover <122508461+jvlover@users.noreply.github.com>
2023-09-07 17:45:36 +0900

    [unrated] Title: 과일 장수, Time: 0.44 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        Arrays.sort(score);
+        int answer = 0;
+        for (int i = score.length-m; i >= 0; i -= m) answer += m*score[i];
+        return answer;
+    }
+}
