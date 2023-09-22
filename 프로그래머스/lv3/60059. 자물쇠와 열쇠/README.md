01ac84c58021994a7b9b6dca18296382b0ee80aa
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2023-09-22 20:57:06 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.21 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/README.md
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/README.md
@@ -1,10 +1,10 @@
 # [level 3] 자물쇠와 열쇠 - 60059 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60059) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60059?language=java) 
 
 ### 성능 요약
 
-메모리: 76.5 MB, 시간: 0.21 ms
+메모리: 73.4 MB, 시간: 0.21 ms
 
 ### 구분
 
--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -5,78 +5,78 @@ class Solution {
         N = lock.length;
         M = key.length;
         len = N+(M-1)*2;
-        
+
         this.key = key;
         this.lock = lock;
-        
+
         makeMap();
-        
+
         boolean answer = checkMap();
-        
+
         return answer;
     }
-    
+
     void makeMap() {
-        
+
         map = new int[len][len];
-        
+
         for (int i = M-1; i < M+N-1; ++i)
             for (int j = M-1; j < M+N-1; ++j)
                 map[i][j] = lock[i-M+1][j-M+1];
     }
-    
+
     boolean checkMap() {
-        
+
         for (int i = 0; i < 4; ++i) {
             if (check()) return true;
             rotateMap();
         }
-        
+
         return false;
     }
-    
+
     boolean check() {
-        
+
         for (int i = 0; i < len-M+1; ++i) {
             for (int j = 0; j < len-M+1; ++j) {
-                
+
                 for (int k = 0; k < M; ++k)
                     for (int l = 0; l < M; ++l)
                         map[i+k][j+l] += key[k][l];
-                
+
                 if (checkAllFit()) return true;
-                
+
                 for (int k = 0; k < M; ++k)
                     for (int l = 0; l < M; ++l)
                         map[i+k][j+l] -= key[k][l];
             }
         }
-        
+
         return false;
     }
-    
+
     boolean checkAllFit() {
-        
+
         for (int i = M-1; i < N+M-1; ++i)
             for (int j = M-1; j < N+M-1; ++j)
                 if (map[i][j] != 1) return false;
-            
+
         return true;
     }
-    
+
     void rotateMap() {
-        
+
         int[][] copyMap = new int[len][len];
-        
+
         for (int i = 0; i < len; ++i)
             for (int j = 0; j < len; ++j)
                 copyMap[i][j] = map[len-j-1][i];
-        
+
         copy(copyMap);
     }
-    
+
     void copy(int[][] copyMap) {
-        
+
         for (int i = 0; i < len; ++i)
             map[i] = copyMap[i].clone();
     }
