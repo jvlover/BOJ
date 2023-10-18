44d99564a61b44844e76daee178397c5f35f40a7
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2023-10-18 09:10:58 +0900

    [Silver II] Title: 한 줄로 서기, Time: 236 ms, Memory: 18156 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; ++i) arr[i] = Integer.parseInt(st.nextToken());
+        List<Integer> list = new ArrayList<>();
+        for (int i = N-1; i >= 0; --i) list.add(arr[i], i+1);
+        list.forEach(i -> sb.append(i).append(' '));
+        System.out.print(sb);
+    }
+}
