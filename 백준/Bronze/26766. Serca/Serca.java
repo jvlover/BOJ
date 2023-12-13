ff3af76f2ef434bc1ed2a34c7a4fc23143111e67
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2023-12-13 13:25:21 +0900

    [Bronze V] Title: Serca, Time: 236 ms, Memory: 90084 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        while (N-->0) {
+            sb.append(" @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @     \n");
+        }
+        System.out.print(sb);
+    }
+}
