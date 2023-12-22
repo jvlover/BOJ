3e2e915715532659bd0abcb3ab8642062d04d80b
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2023-12-22 22:07:37 +0900

    [Bronze IV] Title: 10부제, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        while (st.hasMoreTokens())
+            if (N == Integer.parseInt(st.nextToken())) ++cnt;
+        System.out.print(cnt);
+    }
+}
