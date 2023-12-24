f423ae3663364005c720331028f4bef445714f7c
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2023-12-24 13:28:26 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,9 @@
+s = input()[0]
+if s == 'N':
+    print('North London Collegiate School')
+elif s == 'B':
+    print('Branksome Hall Asia')
+elif s == 'K':
+    print('Korea International School')
+else:
+    print('St. Johnsbury Academy')
