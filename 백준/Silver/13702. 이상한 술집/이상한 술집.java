3ac2c6c0a06d735fe01d20f4ece90e16de4ad04d
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2024-01-30 17:27:25 +0900

    [Silver III] Title: 이상한 술집, Time: 136 ms, Memory: 15268 KB -BaekjoonHub

---
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; ++i) arr[i] = Integer.parseInt(br.readLine());
+        long start = 0, end = Integer.MAX_VALUE;
+        while (start <= end) {
+            long mid = (start+end)/2;
+            int cnt = 0;
+            for (int i = 0; i < N; ++i) cnt += arr[i]/mid;
+            if (cnt >= K) start = mid+1;
+            else end = mid-1;
+        }
+        System.out.print(start-1);
+    }
+}
