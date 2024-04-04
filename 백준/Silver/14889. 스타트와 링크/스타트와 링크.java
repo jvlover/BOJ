63e56dbe7305ecdf44dac8ab676a688086d18b4e
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2024-04-04 20:32:40 +0900

    [Silver I] Title: 스타트와 링크, Time: 104 ms, Memory: 13116 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,38 +1,40 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int[][] map;
-    static boolean[] visit;
-    static int N, team, ans;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visit = new boolean[N];
-        for (int i = 0; i < N; ++i) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; ++j) map[i][j] = Integer.parseInt(st.nextToken());
-        }
-        team = N/2;
-        ans = Integer.MAX_VALUE;
-        dfs(0, 0);
-        System.out.print(ans);
-    }
-    static void dfs(int cnt, int idx) {
-        if (cnt == team) {
-            int a = 0, b = 0;
-            for (int i = 0; i < N; ++i)
-                for (int j = 0; j < N; ++j)
-                    if (visit[i] && visit[j]) a += map[i][j];
-                    else if (!visit[i] && !visit[j]) b += map[i][j];
-            ans = Math.min(ans, Math.abs(a-b));
-            return;
-        }
-        if (idx == N) return;
-        visit[idx] = true;
-        dfs(cnt+1, idx+1);
-        visit[idx] = false;
-        dfs(cnt, idx+1);
-    }
+	static int N, min;
+	static int[] sumR, sumC;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		sumR = new int[N];
+		sumC = new int[N];
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				sumR[i] += num;
+				sumC[j] += num;
+				sum += num;
+			}
+		}
+		min = Integer.MAX_VALUE;
+		choice(0, 0, sum);	
+		System.out.print(min);
+	}
+	static void choice(int idx, int cnt, int sum) {
+		if (cnt == N/2) {
+			min = Math.min(min, Math.abs(sum));
+			if (sum == 0) {
+				System.out.print(0);
+				System.exit(0);
+			}
+			return;
+		}
+		if (idx == N) return;
+		choice(idx+1, cnt+1, sum-sumR[idx]-sumC[idx]);
+		choice(idx+1, cnt, sum);
+	}
 }
