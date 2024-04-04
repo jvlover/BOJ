7feb683cc577a7134dd9fbca7cea5b190c10a379
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2024-04-05 00:11:59 +0900

    [Gold II] Title: 문제 추천 시스템 Version 2, Time: 732 ms, Memory: 77748 KB -BaekjoonHub

---
+++ 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Problem implements Comparable<Problem> {
+        int idx, level, algo;
+        Problem(int idx, int level, int algo) {
+            this.idx = idx;
+            this.level = level;
+            this.algo = algo;
+        }
+        @Override
+        public int compareTo(Problem o) {
+            if (this.level == o.level) return Integer.compare(o.idx, this.idx);
+            return Integer.compare(o.level, this.level);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        TreeSet<Problem> ts = new TreeSet<>();
+        Map<Integer, TreeSet<Problem>> treeSetMap = new HashMap<>();
+        Map<Integer, Integer> levelMap = new HashMap<>(), groupMap = new HashMap<>();
+        while (N-->0) {
+            st = new StringTokenizer(br.readLine());
+            int P = Integer.parseInt(st.nextToken()), L = Integer.parseInt(st.nextToken()), G = Integer.parseInt(st.nextToken());
+            Problem prob = new Problem(P, L, G);
+            ts.add(prob);
+            treeSetMap.putIfAbsent(G, new TreeSet<>());
+            treeSetMap.get(G).add(prob);
+            levelMap.put(P, L);
+            groupMap.put(P, G);
+        }
+        int M = Integer.parseInt(br.readLine());
+        while (M-->0) {
+            st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "recommend": {
+                    int G = Integer.parseInt(st.nextToken()), x = Integer.parseInt(st.nextToken());
+                    sb.append(x == 1 ? treeSetMap.get(G).first().idx : treeSetMap.get(G).last().idx).append('\n');
+                    break;
+                }
+                case "recommend2": {
+                    int x = Integer.parseInt(st.nextToken());
+                    sb.append(x == 1 ? ts.first().idx : ts.last().idx).append('\n');
+                    break;
+                }
+                case "recommend3": {
+                    int x = Integer.parseInt(st.nextToken()), L = Integer.parseInt(st.nextToken());
+                    if (x == 1) {
+                        Problem floor = ts.floor(new Problem(0, L, 0));
+                        sb.append(floor == null ? -1 : floor.idx);
+                    } else {
+                        Problem ceiling = ts.ceiling(new Problem(0, L, 0));
+                        sb.append(ceiling == null ? -1 : ceiling.idx);
+                    }
+                    sb.append('\n');
+                    break;
+                }
+                case "add": {
+                    int P = Integer.parseInt(st.nextToken()), L = Integer.parseInt(st.nextToken()), G = Integer.parseInt(st.nextToken());
+                    Problem prob = new Problem(P, L, G);
+                    ts.add(prob);
+                    treeSetMap.putIfAbsent(G, new TreeSet<>());
+                    treeSetMap.get(G).add(prob);
+                    levelMap.put(P, L);
+                    groupMap.put(P, G);
+                    break;
+                }
+                case "solved": {
+                    int P = Integer.parseInt(st.nextToken()), L = levelMap.get(P), G = groupMap.get(P);
+                    Problem prob = new Problem(P, L, G);
+                    ts.remove(prob);
+                    treeSetMap.get(G).remove(prob);
+                    levelMap.remove(P);
+                    groupMap.remove(P);
+                    break;
+                }
+            }
+        }
+        System.out.print(sb);
+    }
+}
