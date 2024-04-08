102737708b1675f6260016159120131dddbe619a
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2024-04-08 20:33:16 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 520 ms, Memory: 76416 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,41 +1,37 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int N;
-	static int[] parents;
-	static boolean[] check;
-	static ArrayList<Integer>[] list;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		parents = new int[N+1];
-		check = new boolean[N+1];
-		list = new ArrayList[N+1];
-		for (int i = 1; i <= N; i++)
-			list[i] = new ArrayList<Integer>();
-		for (int i = 0; i < N-1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			list[A].add(B);
-			list[B].add(A);
-		}
-		for (int i = 1; i <= N; i++)
-			if (!check[i]) dfs(i);
-		for (int i = 2; i <= N; i++)
-			sb.append(parents[i]).append('\n');
-		System.out.println(sb);
-	}
-	static void dfs(int num) {
-		if (check[num]) return;
-		check[num] = true;
-		for (int i : list[num]) {
-			if (!check[i]) {
-				parents[i] = num;
-				dfs(i);
-			}
-		}
-	}
+    static int[] parent;
+    static boolean[] visit;
+    static ArrayList<Integer>[] graph;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        parent = new int[N+1];
+        visit = new boolean[N+1];
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; ++i) graph[i] = new ArrayList<>();
+        for (int i = 1; i < N; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        for (int i = 1; i <= N; ++i)
+            if (!visit[i]) dfs(i);
+        for (int i = 2; i <= N; ++i)
+            sb.append(parent[i]).append('\n');
+        System.out.print(sb);
+    }
+    static void dfs(int now) {
+        if (visit[now]) return;
+        visit[now] = true;
+        for (int next : graph[now]) {
+            if (visit[next]) continue;
+            parent[next] = now;
+            dfs(next);
+        }
+    }
 }
