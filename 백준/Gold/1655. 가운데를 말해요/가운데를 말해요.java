c287fd0d6905e027b862765711da101b9e9f514b
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2024-06-27 23:00:00 +0900

    [Gold II] Title: 가운데를 말해요, Time: 436 ms, Memory: 35360 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,23 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> min = new PriorityQueue<>();
-		PriorityQueue<Integer> max = new PriorityQueue<>((a, b) -> b-a);
-		while (N-->0) {
-			int num = Integer.parseInt(br.readLine());
-			if (min.size() == max.size()) max.add(num);
-			else min.add(num);
-			if (!min.isEmpty() && !max.isEmpty() && min.peek() < max.peek()) {
-				int temp = min.poll();
-				min.add(max.poll());
-				max.add(temp);
-			}
-			sb.append(max.peek()).append('\n');
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> minPq = new PriorityQueue<>();
+        PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+        while (N-->0) {
+            int num = Integer.parseInt(br.readLine());
+            if (minPq.size() == maxPq.size()) maxPq.offer(num);
+            else minPq.offer(num);
+            if (!(minPq.isEmpty() || maxPq.isEmpty()) && minPq.peek() < maxPq.peek()) {
+                maxPq.offer(minPq.poll());
+                minPq.offer(maxPq.poll());
+            }
+            sb.append(maxPq.peek()).append('\n');
+        }
+        System.out.print(sb);
+    }
 }
