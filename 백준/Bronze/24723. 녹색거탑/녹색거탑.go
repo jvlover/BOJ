5ccb83742a9dcc637eab56d4cf4bb7a93b35c69b
Minhyuk Park <122508461+jvlover@users.noreply.github.com>
2024-08-11 23:19:14 +0900

    [Bronze IV] Title: 녹색거탑, Time: 4 ms, Memory: 856 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.go
@@ -0,0 +1,17 @@
+package main
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+    defer writer.Flush()
+    
+    var n int
+    fmt.Fscan(reader, &n)
+    fmt.Fprintln(writer, 1<<n)
+}
